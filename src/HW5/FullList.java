5038b58d04ee825db8b2a11e91f94c90cf94b5c4
Alexandr 13K <kuchersasha.ruz@gmail.com>
2019-12-17 15:33:13 +0200

    changed fillingList method and add one method

--- src/HW5/FullList.java
+++ src/HW5/FullList.java
@@ -1,19 +1,25 @@
 package HW5;
 
 public class FullList {
-    public void fillingList() {
-        MyList<Integer> IntList = new MyList<>();
+    public void start() {
+        MyList<Integer> arrayList = new MyList<>();
+
+        fillingList(arrayList);
+
+        System.out.println("Maximum Int Value = " + arrayList.returnLargestValue());
+        System.out.println("Minimum Int Value = " + arrayList.returnSmallestValue());
+
+    }
+
+    private void fillingList(MyList<Integer> arrayList) {
         int value1 = -2;
         int value2 = 45;
         int value3 = 30;
         int value4 = -24;
 
-        IntList.add(value1);
-        IntList.add(value2);
-        IntList.add(value3);
-        IntList.add(value4);
-
-        System.out.println("Maximum Int Value = " + IntList.returnLargestValue());
-        System.out.println("Minimum Int Value = " + IntList.returnSmallestValue());
+        arrayList.add(value1);
+        arrayList.add(value2);
+        arrayList.add(value3);
+        arrayList.add(value4);
     }
 }

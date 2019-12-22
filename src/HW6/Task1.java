c88da3b739d0c6a292e11fc9d5493d06b38a4821
Alexandr 13K <kuchersasha.ruz@gmail.com>
2019-12-22 17:24:46 +0200

    Add- {}, and renamed ArrayList

--- src/HW6/Task1.java
+++ src/HW6/Task1.java
@@ -5,19 +5,20 @@ import java.util.List;
 
 class Task1 {
     public static void main(String[] args) {
-        List<Integer> array = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>();
 
-        array.add(3);
-        array.add(9);
-        array.add(11);
-        array.add(18);
-        array.add(20);
-        array.add(22);
+        integers.add(3);
+        integers.add(9);
+        integers.add(11);
+        integers.add(18);
+        integers.add(20);
+        integers.add(22);
 
         System.out.print("Removed numbers: ");
-        for (int num : array) {
-            if (num % 3 == 0)
+        for (int num : integers) {
+            if (num % 3 == 0) {
                 System.out.print(num + " ");
+            }
         }
     }
 }
--- src/HW6/Task3.java
+++ src/HW6/Task3.java
@@ -5,27 +5,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class Task3 {
-     static void main(String[] args) {
-        List<Integer> array1 = new ArrayList<>();
-        List<Integer> array2 = new ArrayList<>();
+    static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
 
-        array1.add(2);
-        array1.add(3);
-        array1.add(6);
-        array1.add(8);
-        array1.add(5);
-        System.out.println("Array1: " + array1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(6);
+        list1.add(8);
+        list1.add(5);
+        System.out.println("list1: " + list1);
 
-        array2.add(21);
-        array2.add(8);
-        array2.add(10);
-        array2.add(5);
-        array2.add(7);
-        System.out.println("Array2: " + array2);
+        list2.add(21);
+        list2.add(8);
+        list2.add(10);
+        list2.add(5);
+        list2.add(7);
+        System.out.println("list2: " + list2);
 
         System.out.print("Similar elements: ");
-        System.out.println(array1.stream()
-                .filter(array2::contains)
+        System.out.println(list1.stream()
+                .filter(list2::contains)
                 .collect(Collectors.toList()));
     }
 }

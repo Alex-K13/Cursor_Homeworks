87ab7cf347c6a5405479791699b68351c4e94989
Alexandr 13K <kuchersasha.ruz@gmail.com>
2020-01-06 22:50:18 +0200

    Add homework 10

---
+++ src/hw10/controller/Remote.java
@@ -0,0 +1,15 @@
+package hw10.controller;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void mute();
+
+    void channelUp();
+}

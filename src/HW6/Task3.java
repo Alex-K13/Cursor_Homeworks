c453822f39f242963ddd8ced27774e0f12c6f96b
Alexandr 13K <kuchersasha.ruz@gmail.com>
2020-01-12 10:13:12 +0200

    fixed bugs of  method

--- src/HW6/Task3.java
+++ src/HW6/Task3.java
@@ -5,29 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class Task3 {
-<<<<<<< HEAD
-     static void main(String[] args) {
-        List<Integer> array1 = new ArrayList<>();
-        List<Integer> array2 = new ArrayList<>();
-
-        array1.add(2);
-        array1.add(3);
-        array1.add(6);
-        array1.add(8);
-        array1.add(5);
-        System.out.println("Array1: " + array1);
-
-        array2.add(21);
-        array2.add(8);
-        array2.add(10);
-        array2.add(5);
-        array2.add(7);
-        System.out.println("Array2: " + array2);
-
-        System.out.print("Similar elements: ");
-        System.out.println(array1.stream()
-                .filter(array2::contains)
-=======
     static void main(String[] args) {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
@@ -49,7 +26,6 @@ class Task3 {
         System.out.print("Similar elements: ");
         System.out.println(list1.stream()
                 .filter(list2::contains)
->>>>>>> HW6
                 .collect(Collectors.toList()));
     }
 }

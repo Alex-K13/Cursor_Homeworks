cb3801c8caa79835b9ee0ba016e5b1d6959fb439
Alexandr 13K <kuchersasha.ruz@gmail.com>
2020-02-09 19:10:26 +0200

    Added hw10 bridge patterns

---
+++ src/hw10/controller/TVRemote.java
@@ -0,0 +1,49 @@
+package hw10.controller;
+
+import hw10.module.Device;
+
+public class TVRemote implements Remote {
+     Device device;
+
+    public TVRemote(Device device) {
+        this.device = device;
+    }
+
+    @Override
+    public void power() {
+        System.out.println("Remote: power toggle");
+        if (device.isEnabled()) {
+            device.disable();
+        } else {
+            device.enable();
+        }
+    }
+
+    @Override
+    public void volumeDown() {
+        System.out.println("Remote: volume down");
+        device.setVolume(device.getVolume() - 5);
+    }
+
+    @Override
+    public void volumeUp() {
+        System.out.println("Remote: volume up");
+        device.setVolume(device.getVolume() + 5);
+    }
+
+    @Override
+    public void channelDown() {
+        System.out.println("Remote: channel down");
+        device.setChannel(device.getChannel() - 1);
+    }
+
+    @Override
+    public void channelUp() {
+        System.out.println("Remote: channel up");
+        device.setChannel(device.getChannel() + 1);
+    }
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
